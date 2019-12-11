e7ac0ee0224417d019df356d411de25b90047a38
Werisu <wellysson35@gmail.com>
2019-12-11 15:49:41 -0300

    Iniciando app de conversor de moedas

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+/*
+* App de conversor de moedas, esses valores de conversões estão sendo todos obtidos através da internet.
+* Então quando o nosso app abriu ele já buscou as conversões através de um site através de uma app.
+* Ele utiliza a própria conversão on line e a conversão do momento e então cada vez que você abrir o app
+* ele terá a conversão do momento.
+* */
+
+/**
+ * como funciona o padrão JSON de armazenamento de dados bom padrão, permite que a gente armazena dados em um formato de árvore.
+ * Com o JSON você vai armazenando dados no formato de Maps (chave: valor)
+ */
+
+/**
+ * API: Utilizar o API do HG Finance.
+ * API nada mais é do que uma ponte que eu posso utilizar para obter os dados de um serviço.
+ */
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
